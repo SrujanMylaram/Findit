38a495067df6b9bf65766454dd3a9df8ba0226f1
SrujanMylaram <msrujan442@gmail.com>
2025-09-26 09:45:01 +0530

    Remove extra backend folder from vite-project

--- vite-project/src/pages/AddFoundItemPage.jsx
+++ vite-project/src/pages/AddFoundItemPage.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate} from "react-router-dom";
+import './AddItemForm.css';
 
 function AddFoundItemPage() {
     const Navigate = useNavigate();
--- vite-project/src/pages/AddLostItemPage.jsx
+++ vite-project/src/pages/AddLostItemPage.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
+import './AddItemForm.css';
 
 function AddLostItemPage() { 
 
